4a59093dc4e0b032c782acbb1b028f9b645e7953
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2020-12-30 19:34:12 -0800

    second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip';
+
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+    ipsum
+  </Tooltip>
+)
+
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <Split className='left' flexBasis='2'>
+        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet constcetur, adimama. Incident shoe lace face brace avacado?<br />
+        {/* make another tooltip direct inside the App */}
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+        This is the content for the right Split. Inventory reluctant smurf walker. Karen is {secondTooltip} scram beat it. Beat the mirror into oblivion.
+      </Split>
+    </main>
   );
 }
 
